64ebca0f2008fad020060ed61b27238bab259440
Luong Van Tuan Anh <luongvantuananh@gapo.com.vn>
2021-11-18 08:53:15 +0700

    add longest common prefix

---
+++ longest-common-prefix/target/debug/deps/longest_common_prefix-b5b4cd5f68cd1b7b.d
@@ -0,0 +1,5 @@
+/home/tuananhai/Documents/PROJECT_OWN/excercise_rust/longest-common-prefix/target/debug/deps/longest_common_prefix-b5b4cd5f68cd1b7b: src/main.rs
+
+/home/tuananhai/Documents/PROJECT_OWN/excercise_rust/longest-common-prefix/target/debug/deps/longest_common_prefix-b5b4cd5f68cd1b7b.d: src/main.rs
+
+src/main.rs:
---
+++ longest-common-prefix/target/debug/longest-common-prefix.d
@@ -0,0 +1,1 @@
+/home/tuananhai/Documents/PROJECT_OWN/excercise_rust/longest-common-prefix/target/debug/longest-common-prefix: /home/tuananhai/Documents/PROJECT_OWN/excercise_rust/longest-common-prefix/src/main.rs
