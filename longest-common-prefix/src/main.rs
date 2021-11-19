d4f17546cf45b3e8dcd8457b3fc39d5aa25191f4
Luong Van Tuan Anh <luongvantuananh@gapo.com.vn>
2021-11-19 14:36:11 +0700

    add valid parrentheses

--- longest-common-prefix/src/main.rs
+++ longest-common-prefix/src/main.rs
@@ -1,6 +1,24 @@
+struct Solution;
+
+impl Solution {
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        
+        let min_length = strs.iter().map(|x| x.len()).min().unwrap();
+
+        for i in (1..min_length+1).rev() {
+            let prefix = &strs[0][0..i];
+            if strs.iter().all(|x| x.find(prefix) == Some(0)){  // x.starts_with(prefix) == true
+                return prefix.to_string();
+            }
+
+            }
+
+        String::new()
+        }
+}
 
 fn main() {
     let strs = vec![String::from("facebook"), String::from("factory"), String::from("facy")];
-    let prefix = strs.iter().map(|x| x.len()).min_by(|a, b| a.cmp(b));
-    println!("Prefix = {:?}", prefix)
+    let prefix = Solution::longest_common_prefix(strs);
+    println!("Prefix = {}", prefix)
 }
