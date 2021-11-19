6aca42e76854b43ccccd11338d5b64d6f7ad6d7d
Luong Van Tuan Anh <luongvantuananh@gapo.com.vn>
2021-11-19 14:33:07 +0700

    add valid parrentheses

---
+++ validparentheses/src/main.rs
@@ -0,0 +1,25 @@
+struct Solution;
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut list_of_brackets = Vec::new();
+
+        for c in s.chars() {
+            match c {
+                '(' => list_of_brackets.push(')'),
+                '[' => list_of_brackets.push(']'),
+                '{' => list_of_brackets.push('}'),
+                ')' | ']' | '}' if Some(c) != list_of_brackets.pop() => return false,
+                _ => {}
+            }
+        }
+
+        return list_of_brackets.is_empty()
+    }
+}
+
+
+
+fn main() {
+    let check = Solution::is_valid("[]".to_string());
+    println!("Result = {:?}", check);
+}
